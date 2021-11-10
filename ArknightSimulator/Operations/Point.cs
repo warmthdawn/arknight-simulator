130333ba50602e7718fa1c9807795d90e459398d
takanashi <1303170174@qq.com>
2021-11-10 17:35:37 +0800

    :bug: 添加SkillStatusDecorator

---
+++ ArknightSimulator/Operations/Point.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArknightSimulator.Operations
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ ArknightSimulator/Operator/SkillStatusDecorator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArknightSimulator.Operator
+{
+    public class SkillStatusDecorator: StatusDecorator
+    {
+        public SkillStatusDecorator(IStatus status) : base(status)
+        {
+
+        }
+    }
+}
